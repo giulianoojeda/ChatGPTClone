a388aac0f7b2f0ae10d483c53477b015fff9c11a
Giuliano Ojeda <giuliano@mokensleague.com>
2023-04-20 23:51:47 -0400

    Add documentation to React component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+// Import React hooks for managing state and side effects
 import { useState, useEffect } from "react";
 
+// Define the main component for the ChatGPT interface
 const App = () => {
+  // Declare state variables for managing the user's input, the API's response, the chat history, and the current chat title
+
   const [value, setValue] = useState(null);
   const [message, setMessage] = useState(null);
   const [previousChats, setPreviousChats] = useState([]);
   const [currentTitle, setCurrentTitle] = useState(null);
 
+  // Function for creating a new chat
   const createNewChat = () => {
     setMessage(null);
     setValue("");
     setCurrentTitle(null);
   };
-
+  // Function for handling click events on chat titles in the sidebar
   const handleClick = (uniqueTitle) => {
     setCurrentTitle(uniqueTitle);
     setMessage(null);
     setValue("");
   };
-
+  // Function for sending the user's message to the OpenAI API and updating the state with the API's response
   const getMessages = async () => {
+    // Construct the options object for the API request
     const options = {
       method: "POST",
       body: JSON.stringify({
@@ -29,24 +35,28 @@ const App = () => {
       },
     };
     try {
+      // Send the request to the OpenAI API and await the response
       const response = await fetch(
         "http://localhost:8000/completions",
         options
       );
       const data = await response.json();
+      // Update the state with the response data
       setMessage(data.choices[0].message);
     } catch (error) {
+      // Log any errors that occur during the request
       console.error(error);
     }
   };
 
+  // Use the useEffect hook to update the chat history when the message and currentTitle state variables change
   useEffect(() => {
     if (!currentTitle && value && message) {
       setCurrentTitle(value);
     }
     if (currentTitle && value && message) {
-      setPreviousChats(prevChats => (
-        [...prevChats,
+      setPreviousChats((prevChats) => [
+        ...prevChats,
         {
           title: currentTitle,
           role: "user",
@@ -57,26 +67,28 @@ const App = () => {
           role: message.role,
           content: message.content,
         },
-        ]
-      ));
+      ]);
     }
   }, [message, currentTitle]);
-
-  const currentChat = previousChats.filter(previousChat => previousChat.title === currentTitle);
-  const uniqueTitles = Array.from(
-    new Set(previousChats.map(previousChat => previousChat.title))
+  // Filter the chat history to only show messages for the current chat title
+  const currentChat = previousChats.filter(
+    (previousChat) => previousChat.title === currentTitle
   );
-
+  // Get a list of unique chat titles to display in the sidebar
+  const uniqueTitles = Array.from(
+    new Set(previousChats.map((previousChat) => previousChat.title))
+  );
+  // Render the component's UI
   return (
     <div className="app">
       <section className="side-bar">
         <button onClick={createNewChat}>+ New chat</button>
         <ul className="history">
-          {uniqueTitles?.map((uniqueTitle, index) => 
+          {uniqueTitles?.map((uniqueTitle, index) => (
             <li key={index} onClick={() => handleClick(uniqueTitle)}>
               {uniqueTitle}
             </li>
-          )}
+          ))}
         </ul>
         <nav>
           <p>Made by Gio</p>
@@ -85,12 +97,12 @@ const App = () => {
       <section className="main">
         {!currentTitle && <h1>ChatGPT Clone</h1>}
         <ul className="feed">
-          {currentChat?.map((chatMessage, index) => 
+          {currentChat?.map((chatMessage, index) => (
             <li key={index}>
               <p className="role">{chatMessage.role}</p>
               <p>{chatMessage.content}</p>
             </li>
-          )}
+          ))}
         </ul>
         <div className="bottom-section">
           <div className="input-container">
